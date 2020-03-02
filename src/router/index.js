c9551f8d868d78058e64f1b14f3d48f676dff19e
靳恬 <348358008@qq.com>
2020-03-02 18:41:31 +0800

    商品管理第一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import Welcome from "../components/Welcome.vue"
 import Users from "../components/user/Users.vue"
 import Rights from "../components/power/Rights.vue"
 import Roles from "../components/power/Roles.vue"
+import Cate from "../components/goods/Cate.vue"
+import Params from "../components/goods/Params.vue"
 // 导入全局样式表
 import '../assets/css/global.css'
 
@@ -33,6 +35,12 @@ const routes = [{
     },{
       path:'/roles',
       component:Roles
+    },{
+      path:'/cate',
+      component:Cate
+    },{
+      path:'/params',
+      component:Params
     }]
   }
 ]
