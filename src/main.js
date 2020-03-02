f5246b4f3c7532bcf4b9da1865db0ed97f338877
靳恬 <348358008@qq.com>
2020-03-02 17:41:59 +0800

    登录功能和用户管理

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 导入字体图标
+import '../src/assets/fonts/iconfont.css'
+// import TreeTable from "vue-table-with-tree-grid"
+
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//axios请求拦截
+axios.interceptors.request.use(config =>{
+//为请求头对象，添加Token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+// Vue.component('tree-table',TreeTable)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
---
+++ src/plugins/element.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+// import { Button } from 'element-ui'
+// import {Form,FormItem} from 'element-ui'
+// import {Input} from 'element-ui'
+// //导入弹窗提示组件
+// import {Message} from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Col,
+  Row,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox,
+  Tag,
+  Tree,
+  Select,
+  Option,
+  Cascader,
+  Alert,
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Cascader)
+Vue.use(Alert)
+//将Message挂载为Vue原型上的属性
+Vue.prototype.$message = Message
+//将MessageBox挂载为vue原型上的属性
+Vue.prototype.$confirm = MessageBox.confirm
